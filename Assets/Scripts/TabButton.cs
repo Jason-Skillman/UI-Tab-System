3330f0bb89957a6ef3eb0fad54194654ce9b99a1
Jason <jason_skillman@outlook.com>
2020-03-16 16:50:16 -0400

    Added tab button and group scripts

---
+++ Assets/Scripts/TabButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler {
+
+    public TabGroup tabGroup;
+
+    private Image background;
+
+
+    private void Awake() {
+        background = GetComponent<Image>();
+    }
+
+    private void Start() {
+        tabGroup.Subscribe(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        tabGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        tabGroup.OnTabExit(this);
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        tabGroup.OnTabSelected(this);
+    }
+
+    public void SetBackground(Sprite sprite) {
+        background.sprite = sprite;
+    }
+
+}
---
+++ Assets/Scripts/TabGroup.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabGroup : MonoBehaviour {
+
+    public List<TabButton> tabButtons;
+
+    [Header("Sprites")]
+    public Sprite spriteIdle;
+    public Sprite spriteHover;
+    public Sprite spriteSelected;
+
+    private TabButton selectedTab;
+
+
+    private void Awake() {
+        tabButtons = new List<TabButton>();
+    }
+
+    /// <summary>
+    /// Subscribes the button to this group
+    /// </summary>
+    /// <param name="button"></param>
+    public void Subscribe(TabButton button) {
+        tabButtons.Add(button);
+    }
+
+    public void OnTabEnter(TabButton button) {
+        ResetTabs();
+
+        //Prevent enter event from selected tab
+        if(selectedTab != null && button == selectedTab) return;
+
+        button.SetBackground(spriteHover);
+    }
+
+    public void OnTabExit(TabButton button) {
+        ResetTabs();
+    }
+
+    public void OnTabSelected(TabButton button) {
+        selectedTab = button;
+        ResetTabs();
+        button.SetBackground(spriteSelected);
+    }
+
+    /// <summary>
+    /// Sets all of the buttons background to idle
+    /// </summary>
+    public void ResetTabs() {
+        foreach(TabButton button in tabButtons) {
+            //Dont reset the selected tab
+            if(selectedTab != null && button == selectedTab) continue;
+
+            button.SetBackground(spriteIdle);
+        }
+    }
+
+}
